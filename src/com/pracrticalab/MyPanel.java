c236909b4f08ba8c6c59dab73cf9ffbce63a37ca
Antonio Campos <geckocore03@outlook.com>
2016-09-14 15:47:32 -0500

    Fixed circle strings

--- src/com/pracrticalab/MyPanel.java
+++ src/com/pracrticalab/MyPanel.java
@@ -1,19 +1,20 @@
 package com.pracrticalab;
 
+import com.sun.istack.internal.Nullable;
+
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.FontMetrics;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Dimension;
 import java.awt.Cursor;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 import java.awt.geom.Ellipse2D;
 
 import java.util.List;
@@ -45,14 +46,31 @@ public class MyPanel extends JPanel {
             public void mouseClicked(MouseEvent e) {
                 ellipse2DArrayList.add(new Ellipse2D.Float(e.getX() - (radius / 2), e.getY() - (radius / 2),
                         radius, radius));
-                ellipse2DArrayList.add(new Ellipse2D.Float(e.getX() - (radius / 2), e.getY() - (radius / 2),
-                        radius, radius));
+
                 JLabel nodeText = new JLabel();
-                nodeText.setText(Integer.toString(count));
+                nodeText.setText(Integer.toString(count++));
                 labelArrayList.add(nodeText);
 
+                Ellipse2D current = findEllipse(e.getPoint());
+                int clicks = 0;
+                Point2D auxPoint;
+
+                if (current != null && e.getClickCount() >= 2) {
+                    clicks += 2;
+                    if (clicks == 2)
+                        auxPoint = e.getPoint();
+                    else if (clicks == 4) {
+                        // TODO: addLinea
+
+                        if (Window.DIRECTED) {
+                            // TODO: If is a directed graph
+                        } else {
+                            // TODO: If is not a directed graph
+                        }
+                    }
+                }
+
                 panel.repaint();
-                count++;
             }
 
             @Override
@@ -77,6 +95,14 @@ public class MyPanel extends JPanel {
         });
 
         panel.addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                if(findEllipse(e.getPoint()) == null)
+                    setCursor(Cursor.getDefaultCursor());
+                else
+                    setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+            }
+
             @Override
             public void mouseDragged(MouseEvent e) {
                 if (dragged != null && offset != null) {
@@ -103,18 +129,6 @@ public class MyPanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D graphics2D = (Graphics2D) g.create();
-        Point p = null;
-
-        for (Ellipse2D node : ellipse2DArrayList) {
-            graphics2D.setColor(Color.WHITE);
-            Point to = node.getBounds().getLocation();
-            to.x += radius / 2;
-            to.y += radius / 2;
-            if (p != null)
-                graphics2D.draw(new Line2D.Float(p, to));
-            p = to;
-
-        }
 
         for (Ellipse2D node : ellipse2DArrayList) {
             graphics2D.setColor(new Color(117, 35, 37));
@@ -124,18 +138,22 @@ public class MyPanel extends JPanel {
                 graphics2D.draw(node);
             }
             graphics2D.setColor(Color.BLUE);
-
-            FontMetrics fm = g.getFontMetrics();
-            for (JLabel nText : labelArrayList) {
-                int textWidth = fm.stringWidth(nText.getText());
-                int x = node.getBounds().x;
-                int y = node.getBounds().y;
-                int width = node.getBounds().width;
-                int height = node.getBounds().height;
-                g.drawString(nText.getText(), x + ((width - textWidth)) / 2,
-                        y + ((height - fm.getHeight()) / 2) + fm.getAscent());
-            }
         }
+
+        graphics2D.setColor(Color.WHITE);
+        for (int i = 0; i < labelArrayList.size(); i++)
+            graphics2D.drawString(labelArrayList.get(i).getText(), (int) ellipse2DArrayList.get(i).getX(),
+                    (int) ellipse2DArrayList.get(i).getY());
+
         graphics2D.dispose();
+    }
+
+    @Nullable
+    private Ellipse2D findEllipse(Point2D p) {
+        for (Ellipse2D node : ellipse2DArrayList) {
+            if (node.contains(p))
+                return node;
+        }
+        return null;
     }
 }
--- src/com/pracrticalab/Window.java
+++ src/com/pracrticalab/Window.java
@@ -2,25 +2,55 @@ package com.pracrticalab;
 
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.ButtonGroup;
 
 import java.awt.Dimension;
 import java.awt.Container;
 
 public class Window extends JFrame {
 
+    public static boolean DIRECTED = true;
+
     public Window() {
-        super("Practica 4");
+        super("Grafos");
         initComponents(this);
     }
 
     private void initComponents(JFrame frame) {
-        frame.setSize(new Dimension(400, 300));
+        frame.setSize(new Dimension(800, 600));
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
 
         Container container = frame.getContentPane();
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("Archivo");
+        JMenu graphTypeMenu = new JMenu("Tipo de grafo");
+        ButtonGroup menuGroup = new ButtonGroup();
+        JRadioButtonMenuItem directedMenu = new JRadioButtonMenuItem("Dirigido", true);
+        JRadioButtonMenuItem noDirectedMenu = new JRadioButtonMenuItem("No dirigido");
+        JMenuItem exitMenu = new JMenuItem("Salir");
 
         MyPanel myPanel = new MyPanel();
+
+        menuBar.add(fileMenu);
+        menuGroup.add(directedMenu);
+        menuGroup.add(noDirectedMenu);
+
+        fileMenu.add(graphTypeMenu);
+        graphTypeMenu.add(directedMenu);
+        graphTypeMenu.add(noDirectedMenu);
+        fileMenu.addSeparator();
+        fileMenu.add(exitMenu);
+
+        frame.setJMenuBar(menuBar);
+
+        directedMenu.addActionListener(e -> DIRECTED = true);
+        noDirectedMenu.addActionListener(e -> DIRECTED = false);
+        exitMenu.addActionListener(e -> System.exit(0));
 
         container.add(myPanel);
     }
